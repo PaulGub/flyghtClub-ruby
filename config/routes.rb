662d946cee48cf0b60e73d9b8b79ff723863fb4b
Paul Gubbiotti <90463998+PaulGub@users.noreply.github.com>
2023-05-09 15:48:35 +0200

    flights/bookings (controller + view)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+  get 'flights/index'
+  get 'flights/show'
+  get 'flights/new'
+  get 'flights/create'
+  get 'flights/edit'
+  get 'flights/update'
+  get 'flights/destroy'
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/edit'
+  get 'bookings/update'
+  get 'bookings/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "flights#index"
 end
